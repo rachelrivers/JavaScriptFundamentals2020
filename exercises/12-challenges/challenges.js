2555d7c0d45263d65ad0b5a1c7ea76560324cd56
jamal taylor <jamal.taylor@cengage.com>
2020-10-14 17:45:41 -0400

    added challenges for today

---
+++ exercises/12-challenges/challenges.js
@@ -0,0 +1,30 @@
+/**
+ * Return the length of the longest word in the provided sentence.
+ *
+ * @param {array} arr
+ * @returns {number}
+ *
+ * @example findLongestWordLength("The quick brown fox jumped over the lazy dog") should return 6
+ * @example findLongestWordLength("What is the average airspeed velocity of an unladen swallow") should return 8
+ */
+function findLongestWordLength(str) {}
+
+/**
+ * Find the missing letter in the passed letter range and return it
+ * @param {string} letters
+ * @returns {string} returns the missing letter from the string
+ * @example
+ *   fearNotLetter("abce") should return "d".
+ *   fearNotLetter("abcdefghjklmno") should return "i".
+ *   fearNotLetter("stvwx") should return "u".
+ *   fearNotLetter("bcdf") should return "e".
+ *   fearNotLetter("abcdefghijklmnopqrstuvwxyz") should return undefined.
+ *
+ */
+function fearNotLetter(str) {
+  return str;
+}
+module.exports = {
+  findLongestWordLength,
+  fearNotLetter,
+};
