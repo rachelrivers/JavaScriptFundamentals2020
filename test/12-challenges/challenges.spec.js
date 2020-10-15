455d59ca01a62939f5cf1e27ec4bf96a6f49ec43
Matina <matinaspatsos@gmail.com>
2020-10-14 20:23:33 -0400

    Updates to challenges test

--- test/12-challenges/challenges.spec.js
+++ test/12-challenges/challenges.spec.js
@@ -6,21 +6,25 @@ const {
   fearNotLetter,
 } = require("../../exercises/12-challenges/challenges");
 
-describe("12-challenges", () => {
-  it("Return the length of the longest word in the provided sentence", () => {
-    const ans = findLongestWordLength(
-      "The quick brown fox jumped over the lazy dog"
-    );
-    expect(ans).to.equal(6);
-    const ansTwo = findLongestWordLength(
-      "What is the average airspeed velocity of an unladen swallow"
-    );
-    expect(ansTwo).to.equal(8);
+describe("exercises/12-challenges/challenges.js", () => {
+  describe("findLongestWordLength()", () => {
+    it("Return the length of the longest word in the provided sentence", () => {
+      const ans = findLongestWordLength(
+        "The quick brown fox jumped over the lazy dog"
+      );
+      expect(ans).to.equal(6);
+      const ansTwo = findLongestWordLength(
+        "What is the average airspeed velocity of an unladen swallow"
+      );
+      expect(ansTwo).to.equal(8);
+    });
   });
-  it("Finds the missing letter in the passed letter range and return it", () => {
-    expect(fearNotLetter("abce").to.equal("d"));
-    expect(fearNotLetter("abcdefghjklmno").to.equal("i"));
-    expect(fearNotLetter("stvwx").to.equal("u"));
-    expect(fearNotLetter("abcdefghijklmnopqrstuvwxyz").to.equal("undefined"));
+  describe("fearNotLetter()", () => {
+    it("Finds the missing letter in the passed letter range and return it", () => {
+      expect(fearNotLetter("abce")).to.equal("d");
+      expect(fearNotLetter("abcdefghjklmno")).to.equal("i");
+      expect(fearNotLetter("stvwx")).to.equal("u");
+      expect(fearNotLetter("abcdefghijklmnopqrstuvwxyz")).to.be.undefined;
+    });
   });
 });
