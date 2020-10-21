d7200318d33f86d9dd004b0e89941c70795e9137
Matina <matinaspatsos@gmail.com>
2020-10-21 19:15:25 -0400

    Transforming DOM exercises

---
+++ exercises/17-transforming-dom/transforming-dom.js
@@ -0,0 +1,5 @@
+/**
+ * You will follow the instructions in the exercises/17-transforming-dom/transforming-dom.html
+ * to transform various elements on the page in different ways.
+ * You may modify the HTML to add ids, classes, data attributes, etc.
+ */
