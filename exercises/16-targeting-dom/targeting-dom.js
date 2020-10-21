a03e16872d7a0d504355d1aa596bab2a500ecd45
Matina <matinaspatsos@gmail.com>
2020-10-21 18:22:46 -0400

    Targeting DOM exercises

---
+++ exercises/16-targeting-dom/targeting-dom.js
@@ -0,0 +1,12 @@
+/**
+ * You will be targeting DOM elements (the HTML) on the page using
+ * "document.querySelector" or "document.querySelectorAll"
+ * You will then use "console.log" to print the results.
+ * 
+ * @example console.log( document.querySelector("#myTarget") );
+ * 
+ * When you use "document.querySelectorAll", you must loop through each
+ * element in the collection and console.log each element. (See the slides)
+ * 
+ * Do not change the HTML unless you are instructed to do so.
+ */
